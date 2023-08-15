0bdd9c67d5733823330ee4d72320238951f16f15
Arundhathi-Nair-S <arundhathin2706@gmail.com>
2023-08-15 12:08:35 +0530

    sample answers

--- server.py
+++ server.py
@@ -1,3 +1,10 @@
+from flask import Flask, request, jsonify
+from dataclasses import dataclass
+
+@dataclass
+class Result:
+    result: int
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,17 +12,22 @@ app = Flask(__name__)
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    g = "Hello world!"
-    return g
+    return 'Hello world!'
+    return ''
 
 @app.route("/calculator/add", methods=['POST'])
-def add(a,b):
-    return a+b
+def add():
+    numbers = request.json
+    response = Result(numbers['first'] + numbers['second'])
+    return jsonify(response)
+    return ''
 
 @app.route("/calculator/subtract", methods=['POST'])
-def subtract(a,b):
-    val = a-b
-    return val
+def subtract():
+    numbers = request.json
+    response = Result(numbers['first'] - numbers['second'])
+    return jsonify(response)
+    return ''
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
